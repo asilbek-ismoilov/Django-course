c72d01116a30f97b1e517bac2d8208e1da6a8973
asilbek-ismoilov <asilbe.m00d@gmail.com>
2025-03-21 17:53:35 +0500

    4 - dars

--- 4 - dars/myapp/forms.py
+++ 4 - dars/myapp/forms.py
@@ -3,9 +3,17 @@ from .models import Student
 
 class StudentForm(forms.ModelForm):
     class Meta:
-        model = Student
+        model = Student 
         fields = ['first_name', 'last_name', 'age', 'email']
 
 
 # forms.ModelForm — Modeldan formani avtomatik yaratadi.
 # Create - Yangi ma’lumot qo‘shish
+
+# Django’da class Meta maxsus klass bo‘lib, u model yoki forma haqida qo‘shimcha ma’lumot (metadata) saqlash uchun ishlatiladi. 
+# Bu Django modeli yoki formasining ishlash qoidalarini belgilash imkonini beradi.
+
+# Meta klassi asosan Django Model va ModelForm larida ishlatiladi.
+
+# model = Student – Qaysi model asosida forma yaratilishini bildiradi.
+# fields = ['name', 'email', 'age'] – Qaysi maydonlar formaga kiritilishi kerakligi
--- 4 - dars/myapp/models.py
+++ 4 - dars/myapp/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Student(models.Model):
-    first_name = models.CharField(max_length=100)  # Talabaning ismi
-    last_name = models.CharField(max_length=100)   # Talabaning familiyasi
-    age = models.IntegerField()                    # Yoshi
-    email = models.EmailField(unique=True)         # Email
+    first_name = models.CharField(max_length=100)  
+    last_name = models.CharField(max_length=100)   
+    age = models.IntegerField()               
+    email = models.EmailField(unique=True)    
     joined_date = models.DateTimeField(auto_now_add=True)  # Qo‘shilgan sana
 
     def __str__(self):
